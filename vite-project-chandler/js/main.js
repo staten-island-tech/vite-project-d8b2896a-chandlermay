ab75f98c6e8879d5f0f2c4fc8673a301579e0cef
chandlermay <133597587+chandlermay@users.noreply.github.com>
2023-11-27 13:15:12 -0500

    1127

--- vite-project-chandler/js/main.js
+++ vite-project-chandler/js/main.js
@@ -2,14 +2,14 @@ import "../style/style.css";
 import { DOMselectors } from "./doms";
 import {songs} from "./songs";
 
-function createCard(songs) {
-  const card = `
-  <div class="card">
-        <h4>${songs.name}</h4>
-        <img src=${songs.image}>
-      </div>
-  `
-  songs.forEach(song => {
-    DOMselectors.container.insertAdjacentElement("afterbegin",card)
-  });
-};
+function createCard(arr) {
+    arr.forEach((el)=>
+      DOMselectors.container.insertAdjacentElement("afterbegin",`
+      <div class="card">
+          <h4>${arr.name}</h4>
+          <h5>${arr.artist}</h5>
+          <img src=${arr.image}>
+        </div>
+      `));
+  };
+  createCard(songs);
